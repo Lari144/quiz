e25ec33080ff6581f1ac029b527f1970f1e119bd
Lari144 <55692422+Lari144@users.noreply.github.com>
2024-03-08 19:10:37 +0100

    Added sortable list

--- quiz-app/components/dragDrop.vue
+++ quiz-app/components/dragDrop.vue
@@ -1,12 +1,34 @@
 <template>
-  <draggable v-model="questions" handle=".handle">
-    <template #item="{ element: question }">
-      <div class="item">
-        <div class="title">{{ question.title }}</div>
-        <div class="handle">Sort</div>
-      </div>
-    </template>
-  </draggable>
+  <div class="questions-container">
+    <div v-for="question in questions" :key="question.id" class="question-item">
+      <div class="title">{{ question.text }}</div>
+      <draggable
+        class="answers-slot"
+        :list="filteredAnswers(question.id)"
+        group="answers"
+        item-key="id"
+      >
+        <template #item="{ element: answer }">
+          <div class="answer">{{ answer.text }}</div>
+        </template>
+        <div v-if="!filteredAnswers(question.id).length" class="placeholder">
+          Drop an answer here
+        </div>
+      </draggable>
+    </div>
+  </div>
+  <div v-for="question in questions" :key="question.id" class="question-item">
+    <draggable
+      class="answers-slot"
+      :list="filteredAnswers(question.id)"
+      group="answers"
+      item-key="id"
+    >
+      <template #item="{ element: answer }">
+        <div class="global-answer-item">{{ answer.text }}</div>
+      </template>
+    </draggable>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,6 +39,10 @@ const questions = ref([]);
 const answers = ref([]);
 const supabase = useSupabaseClient();
 const { cardId } = useBoxStore();
+
+const filteredAnswers = (questionId) => {
+  return answers.value.filter((answer) => answer.question_id === questionId);
+};
 
 const fetchData = async () => {
   questions.value = await fetchRecordsQuestions(supabase, cardId);
